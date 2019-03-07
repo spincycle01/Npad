539f1911469d903b9cced494aacb8ec01496ae3d
Adam <spincycle01@yahoo.com>
2019-03-07 00:11:14 -0500

    minimize works, need API, close btn, relocate at minimize

--- main.js
+++ main.js
@@ -4,8 +4,17 @@
   let noteyContainer = document.createElement("div");
   noteyContainer.id = "mydiv";
   noteyContainer.style.position = "fixed";
-  noteyContainer.style.left = "50px";
-  noteyContainer.style.top = "50px";
+
+  let vpWidth = document.documentElement.clientWidth;
+  let vpHeight = document.documentElement.clientHeight;
+
+  // newEl.style.position = 'fixed';
+  // noteyContainer.style.left = vpWidth/2 + 'px';
+  // noteyContainer.style.top = vpHeight/5 + 'px';
+
+  noteyContainer.style.left = "50%";
+  noteyContainer.style.top = "40%";
+  noteyContainer.style.minWidth = "350px";
 
   noteyContainer.style.zIndex = 9;
   noteyContainer.style.backgroundColor = "#f1f1f1";
@@ -15,7 +24,7 @@
   noteyHeader.id="mydivheader";
   noteyHeader.style.padding = "10px";
   noteyHeader.style.cursor = "move";
-  noteyHeader.style.zIndex = 10;
+  noteyHeader.style.zIndex = 20;
   noteyHeader.style.backgroundColor = "#2196F3";
   noteyHeader.style.color = "#fff";
 
@@ -28,6 +37,8 @@
   noteyMin.style.borderRadius = "10px";
   noteyMin.style.border = "solid 1px";
   noteyMin.style.fontSize = "1em";
+  
+
 
   let saveBtn = document.createElement('button');
   saveBtn.id = '#noteypad-save-button'
@@ -39,14 +50,29 @@
   saveBtn.style.border = "solid 1px";
   saveBtn.style.fontSize = "1em";
 
+  let loadBtn = document.createElement('button');
+  loadBtn.id = "load-button";
+  loadBtn.innerHTML = 'load';
+  loadBtn.style.position = 'absolute';
+  loadBtn.style.right = '45px';
+  loadBtn.style.backgroundColor = "#2196F3";
+  loadBtn.style.borderRadius = "10px";
+  loadBtn.style.border = "solid 1px";
+  loadBtn.style.fontSize = "1em";
+
+
   noteyHeader.innerHTML = 'NoteyPad'
   let newEl = document.createElement("textarea");
   newEl.setAttribute("rows", 40);
   newEl.setAttribute("cols", 40);
+  newEl.style.minWidth = "350px";
+  newEl.style.zIndex = 20;
+  newEl.style.display = "initial"
   
  
   noteyHeader.appendChild(noteyMin);
   noteyHeader.appendChild(saveBtn);
+  noteyHeader.appendChild(loadBtn);
   noteyContainer.appendChild(noteyHeader);
   noteyContainer.appendChild(newEl);
   
@@ -60,12 +86,6 @@
   function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     noteyHeader.onmousedown = dragMouseDown;
-    // if (document.getElementById(elmnt.id + "header")) {
-    //   /* if present, the header is where you move the DIV from:*/
-    //   document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    // } else {
-    //   /* otherwise, move the DIV from anywhere inside the DIV:*/
-     // elmnt.onmousedown = dragMouseDown;
   
     function dragMouseDown(e) {
       e = e || window.event;
@@ -98,10 +118,31 @@
     }
   }
   
+    noteyMin.addEventListener("click", function() {
+      if (newEl.style.display === "initial") {
+        console.log("minimize");  
+        newEl.style.display = "none";
+        noteyMin.innerHTML = 'max';
+      } else {
+      console.log("maximize");  
+      newEl.style.display = "initial";
+      noteyMin.innerHTML = 'min';
+      }
+    });
+
 
   saveBtn.addEventListener('click', () => {
     window.localStorage.setItem(`${window.location.href}`,newEl.value)
   });
+
+  loadBtn.addEventListener('click', () => {
+    console.log('getting...')
+    if (window.localStorage.getItem(`${window.location.href}`)){
+      console.log('getting: ',window.localStorage.getItem(`${window.location.href}`),'should be: ',newEl.val)
+      newEl.value = window.localStorage.getItem(`${window.location.href}`)
+    }
+  });
+
 // })
 
 
